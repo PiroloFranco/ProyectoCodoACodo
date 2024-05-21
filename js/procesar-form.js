c658e49b126167357c084fc8de775c4d747a0d29
Carreira-Sabrina <sabrina-viviana-carreira@gmail.com>
2024-05-21 01:56:35 -0300

    Falta validar los radiobutton del formulario de contacto en contacto.html

--- js/procesar-form.js
+++ js/procesar-form.js
@@ -16,8 +16,10 @@ const btnEnviarForm = document.getElementById("btn-enviar-formulario");
 
 //Patrones de Expresiones Regulares (regEx)
 //Que empiece con una letra, seguida de al menos una letra mas, seguida de cero o ningún espacio, seguido de al menos dos letras
-const nombreRegEx = /^[A-Za-zÀ-Öà-öÑñ]{2,40}(\s[A-Za-zÀ-Öà-öÑñ]{2,40})?$/ig;
+const nombreRegEx = /^[A-Za-zÀ-Öà-öÑñ]{2,40}(\s[A-Za-zÀ-Öà-öÑñ]{2,40})?$/i;
+
 const apellidoRegEx = nombreRegEx; // se usa la misma expresión regular
+
 const prefijoRegEx = /^[1-9]\d{1,2}$/; //Empieza con un núm del 1 al 9, y termina con 1 ó 2 dígitos
 const numCelularRegEx = /^[1-9]\d{6,7}$/; //Empieza con un núm del 1 al 9, seguido por 6 ó 7 dígitos
 //Al validador de mails aun le falta impedir mails como jose.....luis@algo.com (no pueden haber mas de un caracter especial repetido)
@@ -26,11 +28,7 @@ const emailRegEx = /[a-z0-9][a-z0-9\-\._]{5,29}@[a-z0-9\-]{3,30}\.{1}([a-z]{2,3}
 
 function validarFormulario(e){
     e.preventDefault();
-    console.log("ALGUIEN HIZO CLICK EN EL DICHOSO BOTON!")
-
-
-    //CUIDADO CON LOS TELÉFONOS !!!!!!!!!!
-
+    
     const txtInputNombre = inputNombre.value.trim();
     const txtInputApellido = inputApellido.value.trim();
     const txtInputPrefijoCel = Number(inputPrefijoCelular.value.trim());
@@ -43,47 +41,70 @@ function validarFormulario(e){
 
     let msjErrorDatos = "";
 
-
-
     //Validar nombre
     if(txtInputNombre === ""  || !nombreRegEx.test(txtInputNombre)){
         datosEnviadosValidos = false;
-        msjErrorDatos += "El nombre no es válido "
+        msjErrorDatos += "El nombre no es válido \n"
     }
     //Validar apellido
     if(txtInputApellido === ""  || !apellidoRegEx.test(txtInputApellido)){
         datosEnviadosValidos = false;
-        msjErrorDatos += "El apellido no es válido "
+        msjErrorDatos += "El apellido no es válido \n"
     }
     //Validar prefijo
     if(txtInputPrefijoCel === ""  || !prefijoRegEx.test(txtInputPrefijoCel)){
         datosEnviadosValidos = false;
-        msjErrorDatos += "El prefijo no es válido "
+        msjErrorDatos += "El prefijo no es válido \n"
     }
     //Validar celular
     if(txtInputNumCel === ""  || !numCelularRegEx.test(txtInputNumCel)){
         datosEnviadosValidos = false;
-        msjErrorDatos += "El número de celular no es válido "
+        msjErrorDatos += "El número de celular no es válido \n"
     }
     //Validar email
     if(txtInputMail === ""  || !emailRegEx.test(txtInputMail)){
         datosEnviadosValidos = false;
-        msjErrorDatos += "El email no es válido "
+        msjErrorDatos += "El email no es válido \n"
     }
+
+    //Validar select "como nos conociste", mas que validar es darle un feedback al usuario :)
+    let opcionConocisteTxt ="";
+    let eleccionComoNosConociste = inputComoNosConociste.options[inputComoNosConociste.selectedIndex].value;
+    console.log(eleccionComoNosConociste);
+    switch (eleccionComoNosConociste) {
+        case "amigos":
+            opcionConocisteTxt = "Que suerte que tus amigos nos conozcan!\n"
+            break;
+        case "Google":
+            opcionConocisteTxt = "Que bueno que Google te haya acercado a nosotros!\n"
+            break;
+        case "Publicidad":
+            opcionConocisteTxt = "Que bueno que nuestra publicidad te haya gustado!\n"
+            break;
+        case "No recuerda":
+            opcionConocisteTxt = "Bueno, no importa que no recuerdes cómo, lo importante es que nos hayas encontrado!\n"
+            break;
+    }
+
+    //Validar radio buttons
+    
+
+
     //Validar comentario
     if(txtCajaComentarios ===""){
         datosEnviadosValidos = false;
         msjErrorDatos += "No seas tímido, no dejes el comentario vacío ! "
     }
 
-    //Acá habría que cambiar el estilo del contenedor del texto tipo rojo para error, verde para ok
-    //Y si quiero cada error en un <p>?
     if(datosEnviadosValidos){
-        displayError.innerText = "GRACIAS POR COMPLETAR EL FORMULARIO";
+        //Acá hay que agregar las opciones a lo que devuelven "como nos conociste + formapreferida contacto"
+        displayError.innerText = opcionConocisteTxt + "¡ GRACIAS POR COMPLETAR EL FORMULARIO !" + "Te contactaremos a la breverdad por ";
+        displayError.classList.add("datos-correctos");
 
         // Acá habría que borrar los datos del formulario o hacer algo con ellos....
     }else{
         displayError.innerText = msjErrorDatos;
+        displayError.classList.add("datos-incorrectos");
     }
 
 }
